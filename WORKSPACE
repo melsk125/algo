ceafc7501f93dda75de9fe525991145ef391c43c
Panot Chaimongkol <mel.sk125@gmail.com>
2023-04-05 20:50:17 +0200

    Introduce absl lib for pretty prints Also make it easy to test multiple kinds of sort functions

--- WORKSPACE
+++ WORKSPACE
@@ -1,6 +1,16 @@
 load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
 git_repository(
     name = "gtest",
     remote = "https://github.com/google/googletest",
     branch = "v1.13.x",
+)
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+  name = "com_google_absl",
+  urls = ["https://github.com/abseil/abseil-cpp/archive/98eb410c93ad059f9bba1bf43f5bb916fc92a5ea.zip"],
+  strip_prefix = "abseil-cpp-98eb410c93ad059f9bba1bf43f5bb916fc92a5ea",
+  sha256 = "aabf6c57e3834f8dc3873a927f37eaf69975d4b28117fc7427dfb1c661542a87"
 )
--- cc/sort-test.cc
+++ cc/sort-test.cc
@@ -1,7 +1,10 @@
 #include <algorithm>
+#include <iostream>
 #include <iterator>
 #include <vector>
 
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
 #include "gmock/gmock.h"
 #include "cc/sort.h"
 
@@ -13,19 +16,42 @@ struct SortInstance {
   const std::vector<int> expected;
 };
 
-void DoSortTest(SortInstance instance) {
+const std::vector<std::function<void(std::vector<int>&)>> SORT_FUNCTIONS =
+    {cc::InsertionSort};
+
+std::string PrintVector(const std::vector<int>& input) {
+  return absl::StrCat("{", absl::StrJoin(input, ","), "}");
+}
+
+void DoSortTest(SortInstance instance,
+                std::function<void(std::vector<int>&)> sort) {
   std::vector<int> input;
   std::copy(instance.input.begin(),
             instance.input.end(),
             std::back_inserter(input));
 
-  cc::InsertionSort(input);
+  sort(input);
+  std::cout << "   input: " << PrintVector(instance.input) << std::endl;
+  std::cout << "  output: " << PrintVector(input) << std::endl;
+  std::cout << "expected: " << PrintVector(instance.expected) << std::endl;
   EXPECT_THAT(input, ElementsAreArray(instance.expected));
+}
+
+void DoAllSortTests(SortInstance instance) {
+  for (auto sort : SORT_FUNCTIONS) {
+    DoSortTest(instance, sort);
+  }
 }
 
 TEST(SortTest, DoTest) {
   std::vector<SortInstance> instances = {
+    {{}, {}},
+    {{2}, {2}},
+    {{0, 2}, {0, 2}},
     {{5, 2, 4, 6, 1, 3}, {1, 2, 3, 4, 5, 6}},
-    {{31, 41, 59, 26, 41, 58}, {26, 31, 41, 41, 58, 59}}
+    {{31, 41, 59, 26, 41, 58}, {26, 31, 41, 41, 58, 59}},
   };
+  for (auto instance : instances) {
+    DoAllSortTests(instance);
+  }
 }
